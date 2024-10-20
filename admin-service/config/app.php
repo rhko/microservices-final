fef5032e7d6cccc4c528f657fe0bb30f69c6c1db
r.titi <r.titi@wewebit.com>
2024-10-20 15:33:20 +0300

    init email-service & fixes

--- admin-service/config/app.php
+++ admin-service/config/app.php
@@ -174,6 +174,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        Rhko\KafkaBridge\KafkaServiceProvider::class,
 
     ],
 
---
+++ email-service/app/Jobs/OrderCompleted.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Mail\Message;
+
+class OrderCompleted implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public $data;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        var_dump('sending emails:');
+
+        \Mail::send('admin', ['order' => $this->data], function (Message $message) {
+            $message->subject('An Order has been completed');
+            $message->to('admin@admin.com');
+        });
+
+        \Mail::send('customer', ['order' => $this->data], function (Message $message) {
+            $message->subject('An Order has been completed');
+            $message->to($this->data['customer_email']);
+        });
+
+        var_dump("Emails Sent.");
+    }
+}
